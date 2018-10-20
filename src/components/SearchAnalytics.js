1be1a97b326dc5595b8ac9ae001ad5957a148d9b
Henry Parker <henryparker594@gmail.com>
2018-10-20 00:46:59 -0400

    add Bar Chart and calculate word frequency

--- src/components/SearchAnalytics.js
+++ src/components/SearchAnalytics.js
@@ -2,24 +2,68 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {changingTerm,startSearchTerm} from '../actions/search';
 import _ from 'lodash';
+import Chart from 'chart.js';
+import {Bar} from 'react-chartjs-2';
 
 export class SearchAnalitics extends Component{
+    componentDidUpdate(){
+        
+    }
 
+    
     render(){
-        let results = this.props && this.props.sentiment.length > 0? 
-        this.props.sentiment.map(result=> 
-        <li class="text-left list-group-item text-primary" >
-        <h1>score : {result.score}</h1>
-        <br/>
-        <h1>negative : {result.negative.length > 0 ? _.join(result.negative, ', ') : <p>none</p> }</h1>
-        <br></br>
-        <h1>positive : {result.positive.length > 0 ? _.join(result.positive, ', ') : <p>none</p>}</h1>
-        </li>) : 
-        <p>no result</p>;
+        let countUniquePositve = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments)  ? _.countBy(this.props.combineSentiments.positiveWords): {};
+        let countPositive = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments)  ? this.props.combineSentiments.positiveWords.length : 0;
+        let countUniqueNegative = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments)   ? _.countBy(this.props.combineSentiments.negativeWords): {};
+        let countNegative = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments) ? this.props.combineSentiments.negativeWords.length : 0;
+        let countScore = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments) ? _.countBy(this.props.combineSentiments.score): {};
+
+        var arr1 = Object.keys(countUniquePositve);
+        var arr2 = arr1.map(function (k) {
+        return countUniquePositve[k];
+        });
+        console.log(countPositive);
+        console.log(countUniquePositve);
+        console.log(countNegative);
+        console.log(countUniqueNegative);
+        console.log(countScore);
+
+        var dynamicColors = function() {
+                var r = Math.floor(Math.random() * 255);
+                var g = Math.floor(Math.random() * 255);
+                var b = Math.floor(Math.random() * 255);
+                return "rgb(" + r + "," + g + "," + b + ")";};
+        let color = [];
+        arr1.forEach(()=>color.push(dynamicColors()));
+        console.log(color);
+        let barData = this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments) ?  {chartData:{
+            labels: arr1,
+            datasets: [{
+                label:'frequency',
+                data: arr2,
+                backgroundColor: color
+            }] }} : {};
+            
+        let barChart = !_.isEmpty(barData) ? <Bar data={barData.chartData} option={{}}></Bar> : <h1>no chart</h1>;
+        // if(this.props && this.props.sentiment.length > 0 && !_.isEmpty(this.props.combineSentiments)){
+        //     let barData = {
+        //         labels: arr1,
+        //         datasets: [{
+        //             label:'no idea what this is',
+        //             data: arr2
+        //         }] } 
+        //     return( <Bar data={barData}></Bar>)
+        // }
+
+    
+        // let countPositve = this.props && 
+        // this.props.sentiment.length > 0 && 
+        // this.props.combineSentiments.score.length > 0 ? _.countBy(this.props.combineSentiments.posiveWords): {};
+        // console.log(countPositve);
+        
         return(
-            <ul>
-                {results}
-            </ul>
+            <div>{barChart}</div>
+            
         )
     }
 }
@@ -31,7 +75,8 @@ const mapDispatchToProps = (dispatch) => ({
 
 const mapStateToProps = (state) => {
     return {
-      sentiment : state.sentiment
+      sentiment : state.sentiment,
+      combineSentiments : state.combineSentiments
     };
   };
 
