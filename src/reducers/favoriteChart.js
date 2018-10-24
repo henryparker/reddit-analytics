6319d4b62e8b1b6adecd06ca8b299836074bdfe6
Henry Parker <henryparker594@gmail.com>
2018-10-24 02:16:05 -0400

    change the way favChart being stored by putting chart of same term together

--- src/reducers/favoriteChart.js
+++ src/reducers/favoriteChart.js
@@ -5,22 +5,66 @@ export default ( state = [] ,action)=>{
     switch(action.type){
         case ADD_SAVED_CHART:
             if(state.length === 0){
+                let term = action.payload.term;
+                let data = action.payload.savedChartData;
+                let temp ={term,
+                            dataSaved: [data]};
                 return[
                     ...state,
-                    action.payload
+                    temp
                 ]
             }else{
-                if(state.some((val)=>{return isEqual(val,action.payload)})){
+                var index;
+                if(state.some((val)=>{return val.dataSaved.some(val1=>{return isEqual(val1,action.payload.savedChartData)})})){
                     return state
+                }else if(state.some((val,i)=>{index = i; return val.term == action.payload.term})){
+                    let term = action.payload.term;
+                    let data = action.payload.savedChartData;
+                    let temp ={term,
+                            dataSaved: [...state[index].dataSaved,data]};
+                    // let temp2 = {dataSaved: [...state[index].dataSaved,data]};
+                    // // return state.map((val,i)=>{if(i == index){
+                    // //     return{
+                    // //         ...state[index].dataSaved,dataSaved:data
+                    // //     }}})
+                    let newArray = state.slice();
+                    newArray.splice(index,1,temp);
+
+                    // console.log("state", state);
+
+                    // console.log("index", index);
+                    // console.log("temp", temp);
+                    // console.log("temp3", temp3);
+
+                    return [...newArray];
+                    // if(state.length===1){
+                    //     return [
+                            
+                    //         temp
+                             
+                    //  ] 
+                    // }else{
+                    //     return [
+                    //        ...state.slice(0,index),
+                    //        temp,
+                    //        ...state.slice(index)
+                           
+                    // ] 
+                    // }
+                   
+                
                 }else{
-                    return [
-                        ...state,
-                        action.payload
-                    ]
+                let term = action.payload.term;
+                let data = action.payload.savedChartData;
+                let temp ={term,
+                            dataSaved: [data]};
+                return[
+                    ...state,
+                    temp
+                ]
                 }
             }
         default:
             return state;
     }
 };
-
